1c6cb419ede1dd98cffe046f469fe1edfade3c2c
conerthecone894 <143015307+conerthecone894@users.noreply.github.com>
2024-01-18 01:28:38 +0000

    Update GoogleBillboard.java
    this *probably* works

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -3,13 +3,33 @@
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
-		System.out.println("Replace this string with your answer!");
+		for(int i = 2; i < e.length(); i++){
+			if (c.isPrime(Long.parseLong(e.substring(i, i+10))) == true){
+				System.out.println(e.substring(i, i+10)); //prints when it finds a prime #
+				break;
+  			}
+		}
 	}
 	
+  public static boolean isPrime(long x){
+	if (x <= 1) {
+      		return false;
+    	  }
 
-	//Finish this function
-	public boolean isPrime(double d){
+  	int factors = 0;
+
+  	for (int i = 1; i <= Math.sqrt(x); i++) {
+    		if (x % i == 0) {
+      			factors++;
+
+      			if (factors > 1) {
+        			return false;
+      			}
+    		}
 	}
+	  
+  	return true;
+  }
+// ^^^ isPrime func ^^^
 
 }
-
